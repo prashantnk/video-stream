f091f971dd340bf184aedc355f608ef23f2ef0c3
prashant <prashantnk2001@gmail.com>
2021-12-14 19:01:32 +0530

    add all route components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>App!</div>
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<StreamList />} />
+                    <Route path="/streams/new" element={<StreamCreate />} />
+                    <Route path="/streams/edit" element={<StreamEdit />} />
+                    <Route path="/Streams/delete" element={<StreamDelete />} />
+                    <Route path="/streams/show" element={<StreamShow />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 export default App;
